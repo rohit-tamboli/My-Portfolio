458eaf592baba6c07f3680d9024a07b661178a13
Rohit Tamboli <115388799+rohit-tamboli@users.noreply.github.com>
2025-09-27 22:42:29 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     return (
         <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portfolio</a>
+      <a className={styles.title} href="#">Portfolio</a>
       <div className={styles.menu}>
         <img className={styles.menuBtn}
           src={ 
